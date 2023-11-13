b2b22b378e6188739cd9e377edeebfad6a6e0209
yellhtetaung <yehtetaung.dev@gmail.com>
2023-11-14 01:50:58 +0630

    deploying: search record form

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: NextRequest) {
             message: 'Login Successfully',
             success: true
         });
-        response.cookies.set({ name: 'token', value: '1234567890', httpOnly: true });
+        response.cookies.set({ name: 'token', value: '1234567890', httpOnly: false });
 
         return response;
     } catch (err: any) {
