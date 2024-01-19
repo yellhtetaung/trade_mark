dcc33fd82319c6c2cf8af528d8ceb70a06084ac8
yellhtetaung <yehtetaung.dev@gmail.com>
2024-01-19 12:52:18 +0630

    development: fixing login api

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -9,13 +9,19 @@ export async function POST(request: NextRequest) {
         const response = await axiosInstance.post('/api/auth/login', { email, password });
         const data = response.data;
 
+        console.log('response => ', response);
+
         if (response.status === 200) {
             const nextResponse = NextResponse.json({ message: data.message, token: data.token }, { status: 200 });
             nextResponse.cookies.set({ name: 'token', value: data.token, httpOnly: true, sameSite: 'strict', expires: new Date(Date.now() + 60 * 60 * 24 * 1000) });
 
             return nextResponse;
         }
+
+        if (response.status === 500) {
+            return null;
+        }
     } catch (err: any) {
-        return NextResponse.json({ error: err }, { status: 500 });
+        return NextResponse.json({ error: err.response.data.message }, { status: 500 });
     }
 }
